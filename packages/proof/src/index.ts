b17fd7eae4254476d31f93eeeb76bcf23f548ffd
LYC386 <alex98605@gmail.com>
2025-04-23 19:45:37 +0800

    refactor(contracts-noir): rebase noir-support for updated contracts & circuit

--- packages/proof/src/index.ts
+++ packages/proof/src/index.ts
@@ -3,6 +3,17 @@ import generateProof from "./generate-proof"
 import generateNoirProof from "./generate-proof-noir"
 import verifyProof from "./verify-proof"
 import verifyNoirProof from "./verify-proof-noir"
+import { SemaphoreNoirBackend, initSemaphoreNoirBackend, getMerkleTreeDepth } from "./semaphore-noir-backend"
 
 export * from "./types"
-export { generateNoirProof, generateProof, packGroth16Proof, unpackGroth16Proof, verifyProof, verifyNoirProof }
+export {
+    generateNoirProof,
+    generateProof,
+    packGroth16Proof,
+    unpackGroth16Proof,
+    verifyProof,
+    verifyNoirProof,
+    SemaphoreNoirBackend,
+    initSemaphoreNoirBackend,
+    getMerkleTreeDepth
+}
