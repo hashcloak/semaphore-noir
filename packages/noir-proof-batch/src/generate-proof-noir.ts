11f4d9282b08f86b5ddc2b710094fd103aed759e
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2025-04-28 17:41:41 -0600

    feat(noir-proof-batch): implement batching for 2^n Semaphore proofs. Verified test output manually
    The test is for 8 Semaphore proofs. This does not yet take into account the keccak flag

---
+++ packages/noir-proof-batch/src/generate-proof-noir.ts
@@ -0,0 +1,263 @@
+import type { Group, MerkleProof } from "@semaphore-protocol/group"
+import type { Identity } from "@semaphore-protocol/identity"
+import { MAX_DEPTH, MIN_DEPTH } from "@semaphore-protocol/utils/constants"
+import { requireDefined, requireNumber, requireObject, requireTypes } from "@zk-kit/utils/error-handlers"
+import type { BigNumberish } from "ethers"
+import { CompiledCircuit, Noir } from "@noir-lang/noir_js"
+import { maybeGetCompiledNoirCircuit, Project } from "@zk-kit/artifacts"
+import { SemaphoreNoirProof } from "@semaphore-protocol/proof"
+import path from "path"
+import fs from "fs"
+import { writeFile, mkdir } from "fs/promises"
+import { spawn, spawnSync } from "child_process"
+import hash from "./hash"
+import toBigInt from "./to-bigint"
+
+// Functionality from https://github.com/AztecProtocol/aztec-packages/blob/master/barretenberg/ts/src/proof/index.ts#L59
+// That is not exported in their module
+function hexToUint8Array(hex: string): Uint8Array {
+    const sanitisedHex = BigInt(hex).toString(16).padStart(64, "0")
+
+    const len = sanitisedHex.length / 2
+    const u8 = new Uint8Array(len)
+
+    let i = 0
+    let j = 0
+    while (i < len) {
+        u8[i] = parseInt(sanitisedHex.slice(j, j + 2), 16)
+        i += 1
+        j += 2
+    }
+
+    return u8
+}
+
+function flattenUint8Arrays(arrays: Uint8Array[]): Uint8Array {
+    const totalLength = arrays.reduce((acc, val) => acc + val.length, 0)
+    const result = new Uint8Array(totalLength)
+
+    let offset = 0
+    for (const arr of arrays) {
+        result.set(arr, offset)
+        offset += arr.length
+    }
+
+    return result
+}
+
+function flattenFieldsAsArray(fields: string[]): Uint8Array {
+    const flattenedPublicInputs = fields.map(hexToUint8Array)
+    return flattenUint8Arrays(flattenedPublicInputs)
+}
+
+function runBB(argsArray: any[]) {
+    console.log(`Running: bb ${argsArray.join(" ")}`)
+    const result = spawnSync("bb", argsArray, { stdio: "inherit" })
+    if (result.status !== 0) {
+        throw new Error(`bb exited with code ${result.status}`)
+    }
+}
+/**
+ * This generates a Semaphore Noir proof; a zero-knowledge proof that an identity that
+ * is part of a group has shared an anonymous message.
+ *
+ * The message may be any arbitrary user-defined value (e.g. a vote), or the hash of that value.
+ * The scope is a value used like a topic on which users can generate a valid proof only once,
+ * for example the id of an election in which voters can only vote once.
+ * The hash of the identity's scope and secret scalar is called a nullifier and can be
+ * used to verify whether that identity has already generated a valid proof in that scope.
+ * The merkleTreeDepth of the tree determines which zero-knowledge artifacts to use to generate the proof.
+ * If it is not defined, the length of the Merkle proof is used to determine the circuit.
+ * Finally, the compiled Noir circuit can be passed directly, or the correct circuit will be fetched.
+ *
+ * Please keep in mind that groups with 1 member or 2 members cannot be considered anonymous.
+ *
+ * @param identity The Semaphore Identity
+ * @param groupOrMerkleProof The Semaphore group or the Merkle proof for the identity
+ * @param message The Semaphore message
+ * @param scope The Semaphore scope
+ * @param merkleTreeDepth The depth of the tree for which the circuit was compiled
+ * @param noirCompiledCircuit The precompiled Noir circuit
+ * By selecting this option, the challenges in the proof will be generated with the keccak hash function instead of poseidon.
+ * @returns The Semaphore Noir proof ready to be verified.
+ */
+export default async function generateNoirProof(
+    identity: Identity,
+    groupOrMerkleProof: Group | MerkleProof,
+    message: BigNumberish | Uint8Array | string,
+    scope: BigNumberish | Uint8Array | string,
+    merkleTreeDepth?: number,
+    noirCompiledCircuit?: CompiledCircuit
+): Promise<SemaphoreNoirProof> {
+    requireDefined(identity, "identity")
+    requireDefined(groupOrMerkleProof, "groupOrMerkleProof")
+    requireDefined(message, "message")
+    requireDefined(scope, "scope")
+
+    requireObject(identity, "identity")
+    requireObject(groupOrMerkleProof, "groupOrMerkleProof")
+    requireTypes(message, "message", ["string", "bigint", "number", "Uint8Array"])
+    requireTypes(scope, "scope", ["string", "bigint", "number", "Uint8Array"])
+
+    if (merkleTreeDepth) {
+        requireNumber(merkleTreeDepth, "merkleTreeDepth")
+    }
+
+    // Message and scope can be strings, numbers or buffers (i.e. Uint8Array).
+    // They will be converted to bigints anyway.
+    message = toBigInt(message)
+    scope = toBigInt(scope)
+
+    let merkleProof
+
+    // The second parameter can be either a Merkle proof or a group.
+    // If it is a group the Merkle proof will be calculated here.
+    if ("siblings" in groupOrMerkleProof) {
+        merkleProof = groupOrMerkleProof
+    } else {
+        const leafIndex = groupOrMerkleProof.indexOf(identity.commitment)
+        merkleProof = groupOrMerkleProof.generateMerkleProof(leafIndex)
+    }
+
+    // If the merkleTreeDepth is not passed, the length of the merkle proof is used.
+    // Note that this value can be smaller than the actual depth of the tree
+    const merkleProofLength = merkleProof.siblings.length
+    if (merkleTreeDepth !== undefined) {
+        if (merkleTreeDepth < MIN_DEPTH || merkleTreeDepth > MAX_DEPTH) {
+            throw new TypeError(`The tree depth must be a number between ${MIN_DEPTH} and ${MAX_DEPTH}`)
+        }
+    } else {
+        merkleTreeDepth = merkleProofLength !== 0 ? merkleProofLength : 1
+    }
+
+    // The index must be converted to a list of indices, 1 for each tree level.
+    // The missing siblings can be set to 0, as they won't be used in the circuit.
+    const merkleProofIndices = []
+    const merkleProofSiblings = merkleProof.siblings
+
+    for (let i = 0; i < merkleTreeDepth; i += 1) {
+        merkleProofIndices.push((merkleProof.index >> i) & 1)
+
+        if (merkleProofSiblings[i] === undefined) {
+            merkleProofSiblings[i] = 0n
+        }
+    }
+
+    // Prepare inputs for Noir program
+    const secretKey = identity.secretScalar.toString() as `0x${string}`
+    // Format to valid input for circuit
+    const hashPath = merkleProofSiblings.map((s) => s.toString() as `0x${string}`)
+
+    // Following the circom related implementation, pass hashes for scope and message
+    const hashedScope = hash(scope).toString() as `0x${string}`
+    const hashedMessage = hash(message).toString() as `0x${string}`
+
+    // TODO change this to os.tmpdir()
+    const tempDir = path.normalize(path.join("./", "semaphore_artifacts"))
+    const timestamp = Date.now()
+    const proofOutputDir = path.join(tempDir, `${timestamp}`)
+    await mkdir(proofOutputDir, { recursive: true })
+    let noir: Noir
+    try {
+        // TODO consider making maybeGetCompiledNoirCircuit return the path instead of the object
+        // If the Noir circuit has not been passed, it will be automatically downloaded.
+        noirCompiledCircuit ??= await maybeGetCompiledNoirCircuit(Project.SEMAPHORE_NOIR, merkleTreeDepth)
+
+        // store the compiledCircuit locally for bb
+        await mkdir(tempDir).catch((err) => {
+            if (err.code !== "EEXIST") throw err
+        })
+        await writeFile(
+            path.join(tempDir, `circuit_${merkleTreeDepth}.json`),
+            JSON.stringify(noirCompiledCircuit as any)
+        )
+
+        // Initialize Noir with the compiled circuit
+        noir = new Noir(noirCompiledCircuit)
+    } catch (err) {
+        throw new TypeError(`Failed to load compiled Noir circuit: ${(err as Error).message}`)
+    }
+
+    // Generate witness
+    const { witness } = await noir.execute({
+        secret_key: secretKey,
+        index_bits: merkleProofIndices,
+        hash_path: hashPath,
+        merkle_proof_length: merkleProofLength,
+        hashed_scope: hashedScope,
+        hashed_message: hashedMessage
+    })
+    // store witness
+    await writeFile(path.join(tempDir, "witness.gz"), witness)
+
+    // TODO cleanup cc calls
+    // start bb_prove
+    const args = [
+        "prove",
+        "--scheme",
+        "ultra_honk",
+        "-b",
+        path.join(tempDir, `circuit_${merkleTreeDepth}.json`),
+        "-w",
+        path.join(tempDir, "witness.gz"),
+        "-o",
+        `${tempDir}/${timestamp}`,
+        "--output_format",
+        "bytes_and_fields",
+        "--honk_recursion",
+        "1",
+        "--recursive",
+        "--init_kzg_accumulator"
+    ]
+
+    const bbProcess = spawn("bb", args)
+    bbProcess.on("error", (err) => {
+        throw new Error(`Failed to start process: ${err.message}`)
+    })
+
+    await new Promise((resolve, reject) => {
+        bbProcess.on("close", (code: number) => {
+            if (code === 0) {
+                resolve(0)
+            } else {
+                reject(new Error(`Process exited with code ${code}`))
+            }
+        })
+    })
+
+    runBB([
+        "write_vk",
+        "-v",
+        "-s",
+        "ultra_honk",
+        "-b",
+        path.join(tempDir, `circuit_${merkleTreeDepth}.json`),
+        "-o",
+        tempDir,
+        "--output_format",
+        "bytes_and_fields",
+        "--honk_recursion",
+        "1",
+        "--init_kzg_accumulator"
+    ])
+
+    // Uncomment for testing
+    // runBB(["verify", "-k", `${tempDir}/vk`, "-p", `${tempDir}/${timestamp}/proof`])
+
+    // Read out the proof data and return
+    // In the Semaphore circuit we have 4 public inputs
+    const publicInputsCount = 4
+    const proofFields = JSON.parse(fs.readFileSync(`${tempDir}/${timestamp}/proof_fields.json`, "utf-8"))
+    const publicInputs = proofFields.slice(0, publicInputsCount)
+    const proofAsFields = [...proofFields.slice(publicInputsCount)]
+
+    return {
+        merkleTreeDepth,
+        merkleProofLength,
+        merkleTreeRoot: merkleProof.root.toString() as `0x${string}`,
+        nullifier: publicInputs[3].toString() as `0x${string}`,
+        message: message.toString() as `0x${string}`,
+        scope: scope.toString() as `0x${string}`,
+        proofBytes: flattenFieldsAsArray(proofAsFields)
+    }
+}
