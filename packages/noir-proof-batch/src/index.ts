29d0c808abd2e5de8f489624653493a9b4897812
LYC386 <alex98605@gmail.com>
2025-05-02 19:03:15 +0800

    fix(noir-proof-batch): update package.json to fix build issue

--- packages/noir-proof-batch/src/index.ts
+++ packages/noir-proof-batch/src/index.ts
@@ -1,4 +1,5 @@
 import batchSemaphoreNoirProofs from "./batch"
+import generateNoirProofFile from "./generate-proof-noir"
 
 export * from "./types"
-export { batchSemaphoreNoirProofs }
+export { batchSemaphoreNoirProofs, generateNoirProofFile }
