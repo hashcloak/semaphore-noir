cc4b17654c72197d1c10a26fccc3f3543ffc93bb
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2025-05-06 18:26:29 -0600

    fix(noir-proof-batch): pR fixes incorporated

--- packages/noir-proof-batch/src/batch-verify.ts
+++ packages/noir-proof-batch/src/batch-verify.ts
@@ -12,17 +12,13 @@ import { Project, maybeGetBatchVkPath } from "@zk-kit/artifacts"
 export default async function verifyBatchProof(proofPath: string, vkPath?: string, keccak?: boolean): Promise<boolean> {
     const finalVkPath = vkPath ?? (await maybeGetBatchVkPath(Project.SEMAPHORE_NOIR))
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         const verifyArgs = ["verify", "--scheme", "ultra_honk", "-k", finalVkPath, "-p", proofPath]
         if (keccak) {
             verifyArgs.push("--oracle_hash", "keccak")
         }
 
         const bbVerifyProcess = spawn("bb", verifyArgs, { stdio: "inherit" })
-
-        bbVerifyProcess.on("error", (err) => {
-            reject(new Error(`Failed to start bb process: ${err.message}`))
-        })
 
         bbVerifyProcess.on("close", (code: number) => {
             resolve(code === 0)
