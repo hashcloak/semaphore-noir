6002b74161f2933bd4b9274aaa6af3e58e4ab06c
LYC386 <alex98605@gmail.com>
2025-05-08 21:52:16 +0800

    fix(contracts-noir): add contract-noir missing files

--- packages/contracts-noir/contracts/base/SemaphoreNoirVerifier.sol
+++ packages/contracts-noir/contracts/base/SemaphoreNoirVerifier.sol
@@ -1,130 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2022 Aztec
-pragma solidity >=0.8.21;
 
-// TODO - how do we incorporate this in the Semaphore contract
-// TODO - currently roots and nullifiers in a batched proof cannot be checked
-uint256 constant N = 2097152;
-uint256 constant LOG_N = 21;
-uint256 constant NUMBER_OF_PUBLIC_INPUTS = 16;
-library BatchHonkVerificationKey {
-    function loadVerificationKey() internal pure returns (Honk.VerificationKey memory) {
-        Honk.VerificationKey memory vk = Honk.VerificationKey({
-            circuitSize: uint256(2097152),
-            logCircuitSize: uint256(21),
-            publicInputsSize: uint256(16),
-            ql: Honk.G1Point({
-                x: uint256(0x224dffa2aaeaedc6b6149fa7a4cb2d2db4b48aa7aebcf12a4eff376ca91907e7),
-                y: uint256(0x04cb894b27e063af560b46c25aa713da6e92ce2205843e532b31c3346d748b33)
-            }),
-            qr: Honk.G1Point({
-                x: uint256(0x0a9a3d8a12af2e2b80e73f7b4fb1a2b76ac3dd8f68ad0138d95e5bd666877d0c),
-                y: uint256(0x1065db107324d016164ccdd9a9372841493046ff7a4d6d8fcbf8208e4eb2233a)
-            }),
-            qo: Honk.G1Point({
-                x: uint256(0x1b247d3663a44c03704ea7e08f608e46bf3ee4402294368f6b5336044336ddf3),
-                y: uint256(0x0935ba6c9da3223fa5cb39217ce0650ca97c4255791a0c1dad0ff4eba3440bb3)
-            }),
-            q4: Honk.G1Point({
-                x: uint256(0x0b542cc196cc8b0eb8b86042ea807fafcd68c0b0e3097570bd9ab388244628b5),
-                y: uint256(0x0fd19ff666baf07b4e749b472f7b7517f1f77ab3edcf7902cb5260f5dfe22952)
-            }),
-            qm: Honk.G1Point({
-                x: uint256(0x05db220d982083e5d8fbb5964ab426853793114cc97784c0662f7fb49093b15a),
-                y: uint256(0x25c91f18c4d5a4e1b44eac89a8373a41efd0de6aff4ff3c7895cb7101c7a31a1)
-            }),
-            qc: Honk.G1Point({
-                x: uint256(0x212e0cf40c1e663429ad834887b6bc6cdcb45f8e2fc34ad9a8dbb2c2088acca4),
-                y: uint256(0x15baba9110cd7bf6157e312b20343721a19734b786b054f8b7798fe84075052e)
-            }),
-            qArith: Honk.G1Point({
-                x: uint256(0x212f1f6c8a813551f1e52ec48c11ec3f3b0fca9997b0581d08bea04ddc0752e0),
-                y: uint256(0x0f637990bdbd5adbd912afd325ab847b6fbb3d28ff034c91c1e68a7eac726185)
-            }),
-            qDeltaRange: Honk.G1Point({
-                x: uint256(0x2dd763eabd10d073f4b249f0ac9f69796ee4dcfaac96d5a23126decc7912a0d0),
-                y: uint256(0x24f49b460ae83eee1d323044f1ca9ffc4c964e1df2e7329b832525e379351d8a)
-            }),
-            qElliptic: Honk.G1Point({
-                x: uint256(0x28054cb80059e066f2f4efa92071cd0f2f33f7d03d31143e9565c827c5586ca8),
-                y: uint256(0x10e11931417ce0a6612890b8ee982beebf6061224abe73ddcf1207efe2ce49e1)
-            }),
-            qAux: Honk.G1Point({
-                x: uint256(0x2d23ee42c21be324ec226f9c7a84fdd0ac32ebd9a08132a26175aa89a8f7e639),
-                y: uint256(0x2db48d08acdceae4beb0df6ffac301c84f9e50b6b3b146333bffbbdd880cf7b6)
-            }),
-            qLookup: Honk.G1Point({
-                x: uint256(0x17c6d9d50e48678a2ac344538de4c7ece661d9ddf8d6ce71e63ee377b120f70f),
-                y: uint256(0x19c51b736e4c5a7d8380246160d19aad54bcdd8f21bebc775e9dfb36b9a73d45)
-            }),
-            qPoseidon2External: Honk.G1Point({
-                x: uint256(0x137a790e4e5fc4e6e3e2c2915904989f85538bd81adcf3a21d179c351cd34ccc),
-                y: uint256(0x0a6b574aa29f7771b627bd5a5c2b78925336e705dd8a85153954b7f7992a8a0c)
-            }),
-            qPoseidon2Internal: Honk.G1Point({
-                x: uint256(0x286835edb3d8659f60690e4b4b44c4fa3e2c735c34d138fe59cbbf7836431d97),
-                y: uint256(0x1525b6fba23e02e5e78e1f41b028aeada5a7ad87f2e52ff76a305a2bf0790a70)
-            }),
-            s1: Honk.G1Point({
-                x: uint256(0x010843239d7b7a2e9c66625f2913feb13e818265bad80a923ca0eaf38161467f),
-                y: uint256(0x141ce316b7ca52edd89b46aae19ffefe70ed9011f8bc88590eaa4f5158a4ee57)
-            }),
-            s2: Honk.G1Point({
-                x: uint256(0x21723f776efeb964ff91ac76e63c4b79b8a22dd373d4848de905c1dd528320ba),
-                y: uint256(0x1930025d49aa27db7b52aad16d9c307d3ec2c8ab38ce9e53252c9bf3c6266305)
-            }),
-            s3: Honk.G1Point({
-                x: uint256(0x0248ab05faae9973fa1717292832ad458657ae33a7da34c092c3837ebd0014d0),
-                y: uint256(0x2e3b09e1da086a912a286a4c6a4dff1c43e9a2129968d95fb701d316fc1e7767)
-            }),
-            s4: Honk.G1Point({
-                x: uint256(0x2acb44e717fff904dc65845d6571bb9d03ae2f9e8f5741b54398002d36f628be),
-                y: uint256(0x17914e40c463428fa4e860bdf2be5a36d9aac2a8abda95dc86934cbdae4edf77)
-            }),
-            t1: Honk.G1Point({
-                x: uint256(0x1f1156b93b4396e0dac3bd312fdc94243cf3e0cfba606d27d5999f4927ff92b3),
-                y: uint256(0x116a7935196d39ea9178a285c53a6b419d9961d76a65ed28914ca5cc3ffd2433)
-            }),
-            t2: Honk.G1Point({
-                x: uint256(0x23aebc5efc1d0e6d03030b242308fdf369409c76a0245d4f389193b554c30065),
-                y: uint256(0x19f38f8e7cf18f375d75db06fca92a0cbfc1214af084c189478e34dc04c77419)
-            }),
-            t3: Honk.G1Point({
-                x: uint256(0x15642d62fc17d119ba4afb77ab424e0a771b5bbb501c75790a1a4e2906931045),
-                y: uint256(0x21cea98314ec6efc5f8f1f648f42a7a5c1396036397af54a729801cc1c37d4e2)
-            }),
-            t4: Honk.G1Point({
-                x: uint256(0x1f3bd0ebf0709ac30745d0dafb183cdd5b4a42e59fe1e447cad24659049d13a7),
-                y: uint256(0x05900180ddd1cec6e340c70c9bff6f16c2efd51d298fee5fce4355fc26890195)
-            }),
-            id1: Honk.G1Point({
-                x: uint256(0x111337dbd2c760ab3c16b736c3e0963fcab131869553ab7269019a852a6e19d5),
-                y: uint256(0x0d8f188d46d4662c6e450723693dabd05cf70547518af7295c0b1665734363c6)
-            }),
-            id2: Honk.G1Point({
-                x: uint256(0x109474fbac03e39a12123247d537b5b2f67a8ab2d56c649c23ead379cae6701a),
-                y: uint256(0x03bb08f5ad597bcc784d8866cebfda656225dbea2977aeaaf8a580fbe3535f83)
-            }),
-            id3: Honk.G1Point({
-                x: uint256(0x09bb85ad242bfd649cede6fd276c495c71ae752a6849552b1cfa147243cf4bb3),
-                y: uint256(0x063a3fef5f97c2b4291837fb473d6ed4f0f8d94462a97dd586a70b32aa12bc51)
-            }),
-            id4: Honk.G1Point({
-                x: uint256(0x2ab7067253088d36c661bd99c9bd367b53f89c7f953bcc775e2bc18209dc9679),
-                y: uint256(0x06f57a8492c43a8ab8d61b828e00010fe074d48f6c99836c769fe43d8ac16fd6)
-            }),
-            lagrangeFirst: Honk.G1Point({
-                x: uint256(0x0000000000000000000000000000000000000000000000000000000000000001),
-                y: uint256(0x0000000000000000000000000000000000000000000000000000000000000002)
-            }),
-            lagrangeLast: Honk.G1Point({
-                x: uint256(0x007280c5169a0e1f859a500138817ec4b6fb9150ed11685a09038ea35070642d),
-                y: uint256(0x16658259e029bb766fe0d828342f9ef7151c97526c14e4531a6b1778adccedf5)
-            })
-        });
-        return vk;
-    }
-}
+import {MAX_DEPTH} from "./Constants.sol";
+import {IVerifier} from "../interfaces/ISemaphoreNoirVerifier.sol";
+import {HonkVerificationKey, G1Point, G1ProofPoint, VerificationKey} from "./HonkVerificationKey.sol";
+
+uint256 constant NUMBER_OF_PUBLIC_INPUTS = 4;
 
 pragma solidity ^0.8.27;
 
@@ -308,57 +189,6 @@ enum WIRE {
 }
 
 library Honk {
-    struct G1Point {
-        uint256 x;
-        uint256 y;
-    }
-
-    struct G1ProofPoint {
-        uint256 x_0;
-        uint256 x_1;
-        uint256 y_0;
-        uint256 y_1;
-    }
-
-    struct VerificationKey {
-        // Misc Params
-        uint256 circuitSize;
-        uint256 logCircuitSize;
-        uint256 publicInputsSize;
-        // Selectors
-        G1Point qm;
-        G1Point qc;
-        G1Point ql;
-        G1Point qr;
-        G1Point qo;
-        G1Point q4;
-        G1Point qLookup; // Lookup
-        G1Point qArith; // Arithmetic widget
-        G1Point qDeltaRange; // Delta Range sort
-        G1Point qAux; // Auxillary
-        G1Point qElliptic; // Auxillary
-        G1Point qPoseidon2External;
-        G1Point qPoseidon2Internal;
-        // Copy cnstraints
-        G1Point s1;
-        G1Point s2;
-        G1Point s3;
-        G1Point s4;
-        // Copy identity
-        G1Point id1;
-        G1Point id2;
-        G1Point id3;
-        G1Point id4;
-        // Precomputed lookup table
-        G1Point t1;
-        G1Point t2;
-        G1Point t3;
-        G1Point t4;
-        // Fixed first and last
-        G1Point lagrangeFirst;
-        G1Point lagrangeLast;
-    }
-
     struct RelationParameters {
         // challenges
         Fr eta;
@@ -372,24 +202,24 @@ library Honk {
 
     struct Proof {
         // Free wires
-        Honk.G1ProofPoint w1;
-        Honk.G1ProofPoint w2;
-        Honk.G1ProofPoint w3;
-        Honk.G1ProofPoint w4;
+        G1ProofPoint w1;
+        G1ProofPoint w2;
+        G1ProofPoint w3;
+        G1ProofPoint w4;
         // Lookup helpers - Permutations
-        Honk.G1ProofPoint zPerm;
+        G1ProofPoint zPerm;
         // Lookup helpers - logup
-        Honk.G1ProofPoint lookupReadCounts;
-        Honk.G1ProofPoint lookupReadTags;
-        Honk.G1ProofPoint lookupInverses;
+        G1ProofPoint lookupReadCounts;
+        G1ProofPoint lookupReadTags;
+        G1ProofPoint lookupInverses;
         // Sumcheck
         Fr[BATCHED_RELATION_PARTIAL_LENGTH][CONST_PROOF_SIZE_LOG_N] sumcheckUnivariates;
         Fr[NUMBER_OF_ENTITIES] sumcheckEvaluations;
         // Shplemini
-        Honk.G1ProofPoint[CONST_PROOF_SIZE_LOG_N - 1] geminiFoldComms;
+        G1ProofPoint[CONST_PROOF_SIZE_LOG_N - 1] geminiFoldComms;
         Fr[CONST_PROOF_SIZE_LOG_N] geminiAEvaluations;
-        Honk.G1ProofPoint shplonkQ;
-        Honk.G1ProofPoint kzgQuotient;
+        G1ProofPoint shplonkQ;
+        G1ProofPoint kzgQuotient;
     }
 }
 
@@ -716,13 +546,13 @@ function bytesToFr(bytes calldata proofSection) pure returns (Fr scalar) {
 }
 
 // EC Point utilities
-function convertProofPoint(Honk.G1ProofPoint memory input) pure returns (Honk.G1Point memory) {
-    return Honk.G1Point({x: input.x_0 | (input.x_1 << 136), y: input.y_0 | (input.y_1 << 136)});
+function convertProofPoint(G1ProofPoint memory input) pure returns (G1Point memory) {
+    return G1Point({x: input.x_0 | (input.x_1 << 136), y: input.y_0 | (input.y_1 << 136)});
 }
 
-function bytesToG1ProofPoint(bytes calldata proofSection) pure returns (Honk.G1ProofPoint memory point) {
+function bytesToG1ProofPoint(bytes calldata proofSection) pure returns (G1ProofPoint memory point) {
     require(proofSection.length == 0x80, "invalid bytes point");
-    point = Honk.G1ProofPoint({
+    point = G1ProofPoint({
         x_0: uint256(bytes32(proofSection[0x00:0x20])),
         x_1: uint256(bytes32(proofSection[0x20:0x40])),
         y_0: uint256(bytes32(proofSection[0x40:0x60])),
@@ -730,12 +560,12 @@ function bytesToG1ProofPoint(bytes calldata proofSection) pure returns (Honk.G1P
     });
 }
 
-function negateInplace(Honk.G1Point memory point) pure returns (Honk.G1Point memory) {
+function negateInplace(G1Point memory point) pure returns (G1Point memory) {
     point.y = (Q - point.y) % Q;
     return point;
 }
 
-function pairing(Honk.G1Point memory rhs, Honk.G1Point memory lhs) view returns (bool) {
+function pairing(G1Point memory rhs, G1Point memory lhs) view returns (bool) {
     bytes memory input = abi.encodePacked(
         rhs.x,
         rhs.y,
@@ -1532,20 +1362,11 @@ library CommitmentSchemeLib {
     }
 }
 
-interface IVerifier {
-    function verify(bytes calldata _proof, bytes32[] calldata _publicInputs) external view returns (bool);
-}
-
 abstract contract BaseHonkVerifier is IVerifier {
     using FrLib for Fr;
-
-    uint256 immutable n;
-    uint256 immutable logN;
     uint256 immutable numPublicInputs;
 
-    constructor(uint256 _n, uint256 _logN, uint256 _numPublicInputs) {
-        n = _n;
-        logN = _logN;
+    constructor(uint256 _numPublicInputs) {
         numPublicInputs = _numPublicInputs;
     }
 
@@ -1557,15 +1378,19 @@ abstract contract BaseHonkVerifier is IVerifier {
     // Number of field elements in a ultra honk zero knowledge proof
     uint256 constant PROOF_SIZE = 440;
 
-    function loadVerificationKey() internal pure virtual returns (Honk.VerificationKey memory);
+    function loadVerificationKey(uint256 merkleTreeDepth) internal virtual returns (VerificationKey memory);
 
-    function verify(bytes calldata proof, bytes32[] calldata publicInputs) public view override returns (bool) {
+    function verify(
+        bytes calldata proof,
+        bytes32[] calldata publicInputs,
+        uint256 merkleTreeDepth
+    ) public override returns (bool) {
         // Check the received proof is the expected size where each field element is 32 bytes
         if (proof.length != PROOF_SIZE * 32) {
             revert ProofLengthWrong();
         }
 
-        Honk.VerificationKey memory vk = loadVerificationKey();
+        VerificationKey memory vk = loadVerificationKey(merkleTreeDepth);
         Honk.Proof memory p = TranscriptLib.loadProof(proof);
 
         if (publicInputs.length != vk.publicInputsSize) {
@@ -1588,11 +1413,12 @@ abstract contract BaseHonkVerifier is IVerifier {
             publicInputs,
             t.relationParameters.beta,
             t.relationParameters.gamma,
-            /*pubInputsOffset=*/ 1
+            /*pubInputsOffset=*/ 1,
+            vk.circuitSize
         );
 
         // Sumcheck
-        bool sumcheckVerified = verifySumcheck(p, t);
+        bool sumcheckVerified = verifySumcheck(p, t, vk.logCircuitSize);
         if (!sumcheckVerified) revert SumcheckFailed();
 
         bool shpleminiVerified = verifyShplemini(p, vk, t);
@@ -1605,7 +1431,8 @@ abstract contract BaseHonkVerifier is IVerifier {
         bytes32[] memory publicInputs,
         Fr beta,
         Fr gamma,
-        uint256 offset
+        uint256 offset,
+        uint256 n
     ) internal view returns (Fr publicInputDelta) {
         Fr numerator = Fr.wrap(1);
         Fr denominator = Fr.wrap(1);
@@ -1629,7 +1456,11 @@ abstract contract BaseHonkVerifier is IVerifier {
         publicInputDelta = FrLib.div(numerator, denominator);
     }
 
-    function verifySumcheck(Honk.Proof memory proof, Transcript memory tp) internal view returns (bool verified) {
+    function verifySumcheck(
+        Honk.Proof memory proof,
+        Transcript memory tp,
+        uint256 logN
+    ) internal view returns (bool verified) {
         Fr roundTarget;
         Fr powPartialEvaluation = Fr.wrap(1);
 
@@ -1721,7 +1552,7 @@ abstract contract BaseHonkVerifier is IVerifier {
 
     function verifyShplemini(
         Honk.Proof memory proof,
-        Honk.VerificationKey memory vk,
+        VerificationKey memory vk,
         Transcript memory tp
     ) internal view returns (bool verified) {
         ShpleminiIntermediates memory mem; // stack
@@ -1733,7 +1564,7 @@ abstract contract BaseHonkVerifier is IVerifier {
 
         // Arrays hold values that will be linearly combined for the gemini and shplonk batch openings
         Fr[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 2] memory scalars;
-        Honk.G1Point[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 2] memory commitments;
+        G1Point[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 2] memory commitments;
 
         mem.posInvertedDenominator = (tp.shplonkZ - powers_of_evaluation_challenge[0]).invert();
         mem.negInvertedDenominator = (tp.shplonkZ + powers_of_evaluation_challenge[0]).invert();
@@ -1813,7 +1644,7 @@ abstract contract BaseHonkVerifier is IVerifier {
             mem.batchedEvaluation,
             proof.geminiAEvaluations,
             powers_of_evaluation_challenge,
-            logN
+            vk.logCircuitSize
         );
 
         // Compute the Shplonk constant term contributions from A₀(±r)
@@ -1826,7 +1657,7 @@ abstract contract BaseHonkVerifier is IVerifier {
         // Compute Shplonk constant term contributions from Aₗ(±r^{2ˡ}) for l = 1, ..., m-1;
         // Compute scalar multipliers for each fold commitment
         for (uint256 i = 0; i < CONST_PROOF_SIZE_LOG_N - 1; ++i) {
-            bool dummy_round = i >= (logN - 1);
+            bool dummy_round = i >= (vk.logCircuitSize - 1);
 
             if (!dummy_round) {
                 // Update inverted denominators
@@ -1852,25 +1683,25 @@ abstract contract BaseHonkVerifier is IVerifier {
         }
 
         // Finalise the batch opening claim
-        commitments[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N] = Honk.G1Point({x: 1, y: 2});
+        commitments[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N] = G1Point({x: 1, y: 2});
         scalars[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N] = mem.constantTermAccumulator;
 
-        Honk.G1Point memory quotient_commitment = convertProofPoint(proof.kzgQuotient);
+        G1Point memory quotient_commitment = convertProofPoint(proof.kzgQuotient);
 
         commitments[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 1] = quotient_commitment;
         scalars[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 1] = tp.shplonkZ; // evaluation challenge
 
-        Honk.G1Point memory P_0 = batchMul(commitments, scalars);
-        Honk.G1Point memory P_1 = negateInplace(quotient_commitment);
+        G1Point memory P_0 = batchMul(commitments, scalars);
+        G1Point memory P_1 = negateInplace(quotient_commitment);
 
         return pairing(P_0, P_1);
     }
 
     // This implementation is the same as above with different constants
     function batchMul(
-        Honk.G1Point[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 2] memory base,
+        G1Point[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 2] memory base,
         Fr[NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 2] memory scalars
-    ) internal view returns (Honk.G1Point memory result) {
+    ) internal view returns (G1Point memory result) {
         uint256 limit = NUMBER_OF_ENTITIES + CONST_PROOF_SIZE_LOG_N + 2;
         assembly {
             let success := 0x01
@@ -1912,8 +1743,14 @@ abstract contract BaseHonkVerifier is IVerifier {
     }
 }
 
-contract BatchHonkVerifier is BaseHonkVerifier(N, LOG_N, NUMBER_OF_PUBLIC_INPUTS) {
-    function loadVerificationKey() internal pure override returns (Honk.VerificationKey memory) {
-        return BatchHonkVerificationKey.loadVerificationKey();
+contract SemaphoreNoirVerifier is BaseHonkVerifier {
+    constructor() BaseHonkVerifier(NUMBER_OF_PUBLIC_INPUTS) {
+        HonkVerificationKey.checkInvariant(MAX_DEPTH);
+    }
+
+    function loadVerificationKey(
+        uint256 merkleTreeDepth
+    ) internal pure override returns (VerificationKey memory honkVk) {
+        honkVk = HonkVerificationKey.loadVerificationKey(merkleTreeDepth);
     }
 }
